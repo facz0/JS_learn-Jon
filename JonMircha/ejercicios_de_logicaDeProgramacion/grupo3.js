21283425467d4af8e704f703de5f485124b2cc95
mormi08 <fabriziocossio99@gmail.com>
2023-09-13 23:44:51 -0500

    finalización del grupo 3 DEMO

--- JonMircha/ejercicios_de_logicaDeProgramacion/grupo3.js
+++ JonMircha/ejercicios_de_logicaDeProgramacion/grupo3.js
@@ -22,8 +22,8 @@ esCapicua(232)
 //ejercicio 3
 function factorial(x){
     let num_factorial = 1
-    for(let i = 0; i <= x; i++){
-        num_factorial += i
+    for(let i = 1; i <= x; i++){
+        num_factorial *= i
     }
     return console.info(num_factorial)
 }
