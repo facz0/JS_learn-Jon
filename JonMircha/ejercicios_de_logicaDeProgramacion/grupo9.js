0bb83c9ac4fcc771e152b93a9aefd6ea05d8451b
mormi08 <fabriziocossio99@gmail.com>
2023-10-02 23:07:48 -0500

    avance del grupo9

---
+++ JonMircha/ejercicios_de_logicaDeProgramacion/grupo9.js
@@ -0,0 +1,11 @@
+class Pelicula{
+    constructor(ID,title,director,estreno,pais,genero,calificacion){
+        this.ID = "ID";
+        this.title = "title"
+        this.director = "director";
+        this.estreno = "estreno";
+        this.pais = "pais";
+        this.genero = "genero";
+        this.calificacion = "calificacion"
+    }
+}
