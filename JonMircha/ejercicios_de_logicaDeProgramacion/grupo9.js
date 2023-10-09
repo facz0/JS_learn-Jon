5084e932dd51ad4f9605f1f90d75798451b58314
fabcoz <fabriziocossio99@gmail.com>
2023-10-09 17:03:29 -0500

    avance de la class pelicula

--- JonMircha/ejercicios_de_logicaDeProgramacion/grupo9.js
+++ JonMircha/ejercicios_de_logicaDeProgramacion/grupo9.js
@@ -1,18 +1,31 @@
 class Pelicula{
-    constructor(ID,title,director,estreno,pais,genero,calificacion){
-        this.ID = "ID";
-        this.title = "title"
+    constructor(id,title,director,estreno,pais,genero,calificacion){
+        this.id = "ID";
+        this.title = "title";
         this.director = "director";
         this.estreno = "estreno";
         this.pais = "pais";
         this.genero = "genero";
         this.calificacion = "calificacion"
 
-        
+        this.validarIMDB(id)
     }
 
-    
+    validarCadena(propiedad, valor){
+        if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`)
+        if (typeof valor !== "string") return console.error(`${propiedad} "${valor}" ingresado no es una cadena de texto`)
+    }
+    validarIMDB(id){
+        if(this.validarCadena("IMBD id", id)){
+            if(/^([a-z]){2}([0-9]){7}$/.test(id)){
+                return console.error(`El ID ingresado no es válido. Debe tener 9 caracteres, los 2 primeros
+                letras minúsculas, los 7 retamtes números`)
+            }
+        }
+    }
 
-}
+    }
 
-const sound = new Pelicula()
+const peli = new Pelicula({
+    id: "33holawer"
+})
