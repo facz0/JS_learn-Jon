435c72497ae395954cd2fa11047fe8aa5785ac12
mormi08 <fabriziocossio99@gmail.com>
2023-10-11 00:32:12 -0500

    avanzanding

--- JonMircha/ejercicios_de_logicaDeProgramacion/grupo9.js
+++ JonMircha/ejercicios_de_logicaDeProgramacion/grupo9.js
@@ -1,7 +1,7 @@
 class Pelicula{
-    constructor({id,title,director,estreno,pais,genero,calificacion}){
+    constructor({id,titulo,director,estreno,pais,genero,calificacion}){
         this.id = id;
-        this.title = title;
+        this.titulo = titulo;
         this.director = director;
         this.estreno = estreno;
         this.pais = pais;
@@ -9,6 +9,9 @@ class Pelicula{
         this.calificacion = calificacion
 
         this.validarIMDB(id)
+        this.validarTitulo(titulo)
+        this.validarDirector(director)
+        this.validarEstreno(estreno)
     }
 
     validarCadena(propiedad,valor){
@@ -17,9 +20,25 @@ class Pelicula{
         return true
     }
     validarLongitudCadena(propiedad, valor, longitud){
-        if(valor.length > longitud) return console.error(`${propiedad} "${valor}" escede el valor de números de caracteres
+        if(valor.length > longitud) return console.error(`${propiedad} "${valor}" excede el valor de números de caracteres
         permitidos (${longitud})`)
+        return true
     }
+    validarNumero(propiedad, valor){
+        if(!valor) return console.warn(`${propiedad} "${valor}" está vacío`)
+        if(typeof valor !== "number") return console.error(`${propiedad} "${valor}" ingresado no es ún número`)
+        return true
+    }
+    validarArreglo(propiedad, valor){
+        if(!valor) return console.warn(`${propiedad} "${valor}" está vacío`)
+        if(!(valor instanceof Array)) return console.error(`${propiedad} "${valor}" ingresado no es un arreglo`)
+        if(valor.length === 0) return console.warn(`${propiedad} "${valor}" no tiene datos`)
+        for(let pais of valor){
+            if(typeof pais !== "string") return console.error(`${propiedad} "${valor}" ingresado no es una cadena de texto`)
+        }
+        return true
+    }
+    
 
     validarIMDB(id){
         if(this.validarCadena("IMBD id", id)){
@@ -30,15 +49,32 @@ class Pelicula{
         }
     }
 
-    validarTitulo(title){
-        if(this.validarCadena("Peli titulo", title)){
-            this.validarLongitudCadena("Peli titulo", )
+    validarTitulo(titulo){
+        if(this.validarCadena("titulo", titulo))
+            this.validarLongitudCadena("titulo", titulo, 100)
+        
+    }
+
+    validarDirector(director){
+        if(this.validarCadena("director", director)){
+            this.validarLongitudCadena("Peli director", director, 50)
+        }
+    }
+
+    validarEstreno(estreno){
+        if(this.validarNumero("Año de Estreno", estreno)){
+            if(!(/^([0-9]){4}$/.test(estreno))){
+                return console.error(`Año de estreno "${estreno}" debe ser un número de 4 dígitos menor a 2024`)
+            }
         }
     }
 
 }
 
 const peli = new Pelicula({
-    id: "tt1234567"
+    id: "tt1234567",
+    titulo: "La gran Sangre",
+    director: "Machín Alcantara",
+    estreno: 2020
 })
 
