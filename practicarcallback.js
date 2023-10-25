a869491e2f15f1e348bbd36007b3a215fa5c8ba1
mormi08 <fabriziocossio99@gmail.com>
2023-10-25 00:12:40 -0500

    asincronismo

---
+++ practicarcallback.js
@@ -0,0 +1,22 @@
+const simple = (tarea) => console.log(tarea)
+const pesada = (tarea) => {
+    console.log(`Empezando Tarea ${tarea}...`)
+    for (let i=0; i<200000;i++){
+        Math.random() - Math.random() * Math.random()
+    }
+    console.log(`Tarea ${tarea} terminada`)
+}
+const tareaPablo = (tarea, callback, tiempo) => {
+    console.log(tarea)
+    setTimeout(callback, tiempo)
+}
+
+const tareas = [
+    () => simple("escribir guión"),
+    () => tareaPablo("hacer miniatura", () => simple("revisar miniatura"), 2000),
+    () => pesada("grabar video"),
+    () => tareaPablo("editar video", () => simple("revisar video"), 5000)
+
+]
+
+for(let tarea of tareas) tarea()
