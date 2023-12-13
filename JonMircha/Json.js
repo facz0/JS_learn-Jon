8a4127d7777652bc341fa0040625dc0df4cac862
mormi08 <fabriziocossio99@gmail.com>
2023-12-13 00:10:07 -0500

    dsfkgd

--- JonMircha/Json.js
+++ JonMircha/Json.js
@@ -10,5 +10,16 @@ const json = {
     nulo: null
 }
 
+//El .parse es para que se imprima el tipo de texto como tal escrito en formato cadena de texto
 console.log(JSON.parse("{}"))
 console.log(JSON.parse("[1,2,3,4]"))
+console.log(JSON.parse("true"))
+console.log(JSON.parse("19"))
+
+//El .stringify convierte un tipo de dato a una cadena de texto
+console.log(JSON.stringify({}));
+console.log(JSON.stringify([1,2,3,4]));
+console.log(JSON.stringify(true));
+console.log(JSON.stringify(null));
+console.log(JSON.stringify(undefined));
+console.log(JSON.stringify({x:2,y:4}));
