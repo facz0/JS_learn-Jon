9c2c5e7175c888fc09b88a6331d48b97dd889d6f
mormi08 <fabriziocossio99@gmail.com>
2024-01-08 23:08:29 -0500

    Dom con CSS

--- JonMircha/dom.js
+++ JonMircha/dom.js
@@ -1,7 +1,7 @@
 // console.log(document.getElementsByTagName("li"))
 // console.log(document.getElementsByClassName("card"))
 // console.log(document.getElementsByName("nombre"))
-// --->
+// ---> Ahora se usa...
 console.log(document.querySelectorAll("a"))
 console.log(document.querySelector(".card"))
 console.log(document.getElementById("menu"))
@@ -12,12 +12,13 @@ console.log(document.querySelector(".link-dom").href)
 console.log(document.querySelector(".link-dom").getAttribute("href"))
 
 //los SETeadores...
+/*cambiando el lang a español*/
 document.documentElement.lang = "es"
 console.log(document.documentElement.lang)
 document.documentElement.setAttribute("lang", "es-PR")
 console.log(document.documentElement.lang)
 
-//variables que guardenelementos del DOM se declaran con el símbolo "$"
+//variables que guarden elementos del DOM se declaran con el símbolo "$"
 const $linkDOM = document.querySelector(".link-dom")
 $linkDOM.setAttribute("target","_blank")
 $linkDOM.setAttribute("rel","noopener")
@@ -29,7 +30,7 @@ console.log($linkDOM.hasAttribute("rel"))
 //Data-Attributes
 console.log($linkDOM.getAttribute("data-description"))
 console.log($linkDOM.dataset.description)
-$linkDOM.setAttribute("data-description","Modelo de Objeto del  Documento")
+$linkDOM.setAttribute("data-description","Modelo de Objeto del Documento")
 console.log($linkDOM.dataset.description)
 $linkDOM.dataset.description = "suscríbete a mi canal y comparte"
 console.log($linkDOM.dataset.description)
--- JonMircha/dom2.js
+++ JonMircha/dom2.js
@@ -10,6 +10,8 @@ $linkDOM.style.setProperty("text-decoration","none")
 $linkDOM.style.setProperty("display","block")
 $linkDOM.style.width = "50%"
 $linkDOM.style.textAlign = "center"
+$linkDOM.style.marginLeft = "auto"
+$linkDOM.style.marginRight = "auto"
 $linkDOM.style.borderRadius = "5rem"
 
 //variables CSS - Custom properties CSS
@@ -27,4 +29,29 @@ $body.style.color = varYellowColor
 $html.style.setProperty("--dark-color","grey")
 varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
 $body.style.setProperty("background-color",varDarkColor)
+
+//CLASES CSS
+const $card = document.querySelector(".card")
+console.log($card)
+console.log($card.className)
+console.log($card.classList)
+console.log($card.classList.contains("rotate-45"))
+/*añadir otra calse mas al elemento que tiene la clase card*/
+$card.classList.add("rotate-45")
+console.log($card.classList.contains("rotate-45"))
+$card.classList.remove("rotate-45")
+/*añade la clase si es que el elemento no la tiene*/
+$card.classList.toggle("rotate-45")
+/*quitamos la clase repitiendo el toggle*/
+$card.classList.toggle("rotate-45")
+console.log($card.classList.contains("rotate-45"))
+$card.classList.toggle("rotate-45")
+/*reemplaza la clase 2da por la 1era*/
+$card.classList.replace("rotate-45", "rotate-135")
+$card.classList.add("opacity-80", "sepia")
+/*removemos*/
+$card.classList.remove("opacity-80", "sepia")
+/*añadimos de nuevo*/
+$card.classList.toggle("opacity-80", "sepia")
+
 
